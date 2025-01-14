375771b3dceb91efabdb46da3783499b664ab574
Pham Tien <phammackimtien@gmail.com>
2025-01-14 18:46:58 +0700

    [Update] Register and Login page

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,7 @@
 import { Button, Divider, Form, Input, message, Typography } from 'antd'
-import backgroundAuth from '@/assets/background.png'
 import { UserOutlined, LockOutlined, GoogleOutlined, FacebookFilled } from '@ant-design/icons'
 import { Link } from 'react-router-dom'
-import signup from '@/assets/signup.jpg'
+import signup from '@/assets/register.jpg'
 
 export default function Register() {
   const register = () => {
@@ -18,28 +17,34 @@ export default function Register() {
           backgroundPosition: 'center'
         }}
       >
-        <Typography.Title style={{ color: 'white', fontWeight: 'bold', marginTop: '50px' }}>
-          Create your account free
-        </Typography.Title>
-        <p className='text-white text-lg mt-4 animate-pulse'>
-          Join our community and unlock unlimited potential with exclusive resources, and opportunities designed just
-          for you.
-        </p>
       </div>
-      <div className='w-1/2 bg-purple-100'>
+      <div className='w-1/2 bg-red-50'>
         <Form
           onFinish={register}
-          className='text-center bg-white bg-opacity-30 p-10'
+          className='text-center bg-white bg-opacity-30 p-10 pt-0 pb-0'
           labelAlign='left'
           labelCol={{ span: 24 }}
           wrapperCol={{ span: 24 }}
         >
-          <Typography.Title>SIGN UP</Typography.Title>
+          <Typography.Title style={{color: '#e57373'}}>SIGN UP</Typography.Title>
           <div className='flex justify-center gap-6 text-gray-600 font-bold text-xl'>
             <GoogleOutlined className='cursor-pointer text-red-500' onClick={register} />
             <FacebookFilled className='cursor-pointer text-blue-900' onClick={register} />
           </div>
           <Divider className='border-black border-solid'>OR</Divider>
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: 'Please enter Fullname'
+              }
+            ]}
+            label='Fullname'
+            name={'myFullName'}
+            className='mb-2 mt-0'
+          >
+            <Input prefix={<UserOutlined />} placeholder='Enter your fullname' allowClear />
+          </Form.Item>
           <Form.Item
             rules={[
               {
@@ -71,23 +76,23 @@ export default function Register() {
             rules={[
               {
                 required: true,
-                message: 'Please enter username'
+                message: 'Please enter valid password'
               }
             ]}
-            label='Username'
-            name={'myUsername'}
+            label='ConfirmPassword'
+            name={'myPassword'}
             className='mb-6'
           >
-            <Input prefix={<UserOutlined />} placeholder='Enter your username' allowClear />
+            <Input.Password prefix={<LockOutlined />} placeholder='Enter your confirm password' allowClear />
           </Form.Item>
-          <Button type='primary' htmlType='submit' block className='bg-black'>
+          <Button type='primary' htmlType='submit' block className='bg-red-400'>
             Register
           </Button>
           <div className='text-center mt-8 flex justify-center items-center gap-2'>
             <span className='text-gray-800'>Already have an account?</span>
             <Button
               type='default'
-              className='border-black border-1 text-black px-4 py-1 hover:bg-black hover:text-white transition-all rounded-md'
+              className='border-red-400 border-1 text-black px-4 py-1 hover:bg-black hover:text-white transition-all rounded-md'
             >
               <Link to='/login'>Login</Link>
             </Button>
